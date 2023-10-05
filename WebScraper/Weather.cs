b358833ca4c53c9fc5cbb07afb255957605f20ed
Piotr Zalejski <piotrzalejski@outlook.com>
2023-10-04 21:05:05 -0500

    refactored code into smaller, more focused methods

--- WebScraper/Weather.cs
+++ WebScraper/Weather.cs
@@ -6,44 +6,61 @@ namespace WebScraper
 {
     class Weather
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // Send get request to weather.com
             String url = "https://weather.com/weather/today/l/35bf30c24f7d2e3d25bbb6f0399afcd2f256c0f54829bea5dc94dee1eacd9cfa";
-            var httpClient = new HttpClient();
-            var html = httpClient.GetStringAsync(url).Result;
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(html);
+            var htmlDocument = await GetHtmlDocAsync(url);
 
-            // Get the temp
-            var tempElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@data-testid='TemperatureValue']");
-            var temperature = tempElement.InnerText.Trim();
-            Console.WriteLine($"Temperature: {temperature}");
+            var temp = GetTemperature(htmlDocument);
+            Console.WriteLine($"Temperature: {temp}");
 
-            // Get the conditions
-            var condElement = htmlDocument.DocumentNode.SelectSingleNode("//div[contains(@class,'CurrentConditions--phrase')]");
-            var condition = condElement.InnerText.Trim();
+            var condition = GetCondition(htmlDocument);
             Console.WriteLine($"Condition: {condition}");
 
-            // Get the location
-            var locElement = htmlDocument.DocumentNode.SelectSingleNode("//h1[contains(@class,'CurrentConditions--location')]");
-            var location = locElement.InnerText.Trim();
-            string[] parts = location.Split(',');
+            var location = GetLocation(htmlDocument);
+            string[] parts = location.Split(",");
             string cityName = parts[0];
             Console.WriteLine($"Location: {location}");
 
-            // initialize new instance of SpeechSynthesizer
-            SpeechSynthesizer synth = new SpeechSynthesizer();
+            SpeakWeather(cityName, temp, condition);
+        }
+        static async Task<HtmlDocument> GetHtmlDocAsync(string url)
+        {
+            using var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            return htmlDocument;
+        }
 
-            // configure audio output
-#pragma warning disable CA1416
+        static string GetTemperature(HtmlDocument htmlDocument)
+        {
+            var tempElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@data-testid='TemperatureValue']");
+            return tempElement?.InnerText.Trim();
+        }
+
+        static string GetLocation(HtmlDocument htmlDocument)
+        {
+            var locElement = htmlDocument.DocumentNode.SelectSingleNode("//h1[contains(@class,'CurrentConditions--location')]");
+            return locElement?.InnerText.Trim();
+        }
+
+        static string GetCondition(HtmlDocument htmlDocument)
+        {
+            var condElement = htmlDocument.DocumentNode.SelectSingleNode("//div[contains(@class,'CurrentConditions--phrase')]");
+            return condElement?.InnerText.Trim();
+        }
+
+        static void SpeakWeather(string location, string temp, string condition)
+        {
+            using var synth = new SpeechSynthesizer();
+
+            //configure audio output
             synth.SetOutputToDefaultAudioDevice();
-            synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.SelectVoiceByHints(VoiceGender.Neutral);
 
-            // Speak
-            synth.Speak(textToSpeak: $"Currently in {cityName} it is {temperature} and {condition}");
-#pragma warning restore CA1416
-
+            //speak
+            synth.Speak($"Currently in {location} it is {temp} and {condition}");
         }
     }
 }
